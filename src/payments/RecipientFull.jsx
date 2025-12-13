4996d69d65c06248b2198f7e7342666a562eaba6
Sezan00 <sezanvay.bd@gmail.com>
2025-12-14 03:55:42 +0600

    initial commit

--- src/payments/RecipientFull.jsx
+++ src/payments/RecipientFull.jsx
@@ -4,9 +4,12 @@ import { useParams } from 'react-router-dom';
 import RecipientFullSkeloton from '../component/skeleton/RecipientFullSkeloton';
 
 export const RecipientFull = () => {
-   const { id } = useParams();
-  const [singleRecipient, setSingleRecipient] = useState(null);
-  const [Loading, setLoading] = useState(true);
+    const { id } = useParams();
+    const [singleRecipient, setSingleRecipient] = useState(null);
+    const [Loading, setLoading] = useState(true);
+    const [editField, setEditField] = useState(null);
+    const [editValue, setEditValue] = useState("");
+
 
   const fetchSingleRecipient = async () =>{
     try{
@@ -33,6 +36,54 @@ export const RecipientFull = () => {
   if(Loading){
     return  <RecipientFullSkeloton/>
   }
+
+
+      const updateRecipient = async (field, value) => {
+    try {
+      const token = localStorage.getItem('token');
+
+      const res = await axios.put(
+        `http://localhost:8000/api/recipient/edit/${id}`,
+        {
+           field: field,
+           value: value,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+        setSingleRecipient(prev => ({
+        ...prev,
+        Recipient: {
+          ...prev.Recipient,
+          [field]: value   
+      }
+    }));
+
+      
+
+      alert("Update successful");
+
+    } catch (err) {
+      console.error(err);
+      alert("Failed");
+    }
+  };
+    const fields = [
+      'receive_type',
+      'full_name',
+      'phone',
+      'email',
+      'city',
+      'address',
+      'bank_name',
+      'bank_account',
+      'wallet_type',
+      'wallet_number',
+    ];
   
   
   return (
@@ -57,63 +108,256 @@ export const RecipientFull = () => {
 
           {/* {singleRecipient.Recipient.map((item)=>(
             <div key={item.id} className="space-y-3"> */}
-          <div className="p-4 bg-gray-100 rounded-xl border cursor-pointer transition-all duration-200 hover:bg-gray-200 hover:shadow-md hover:translate-y-1">
+          
+
+          {/* Receive type section  */}
+          <div className="p-4 bg-gray-100 rounded-xl border cursor-pointer transition-all duration-200 hover:bg-gray-200 hover:shadow-md hover:translate-y-1"
+            onClick={()=>{
+              setEditField('receive_type');
+              setEditValue(singleRecipient?.Recipient?.receive_type);
+            }}
+          >
           <p className="text-gray-500 text-sm">Recipient Type</p>
-          <p className="text-gray-800 font-semibold"> {singleRecipient?.Recipient?.receive_type ?? "Null"}</p>
+            {editField === "receive_type" ? (
+              <input type="text" 
+              className="w-full p-2 border rounded-lg bg-white"
+              autoFocus
+              value={editValue}
+              onChange={(e)=> setEditValue(e.target.value)}
+              onBlur={() => {
+              updateRecipient("receive_type", editValue);
+              setEditField(null);
+            }}                      
+              />
+            ) : (
+              <p className="text-gray-800 font-semibold"> {singleRecipient?.Recipient?.receive_type ?? "Null"}</p>
+            )
+          }
           </div>
 
-
-          <div className="p-4 bg-gray-100 rounded-xl border cursor-pointer ransition-all duration-200 hover:bg-gray-200 hover:shadow-md hover:translate-y-1">
+          {/* Full name section  */}
+          <div className="p-4 bg-gray-100 rounded-xl border cursor-pointer ransition-all duration-200 hover:bg-gray-200 hover:shadow-md hover:translate-y-1"
+          onClick={()=>{
+            setEditField('full_name');
+            setEditValue(singleRecipient?.Recipient?.full_name)
+          }}
+          >
           <p className="text-gray-500 text-sm">Full Name</p>
-          <p className="text-gray-800 font-semibold cursor-pointer">{singleRecipient?.Recipient?.full_name ?? "Null"}</p>
+            {editField === 'full_name' ? (
+              <input
+                className='w-full p-2 border rounded-lg bg-white'
+                autoFocus
+                value={editValue}
+                onChange={(e)=> setEditValue(e.target.value)}
+                 onBlur={() => {
+              updateRecipient("full_name", editValue);
+              setEditField(null);
+            }}
+              />
+            ): (
+               <p className="text-gray-800 font-semibold cursor-pointer">{singleRecipient?.Recipient?.full_name ?? "Null"}</p>
+            )
+            
+          }
           </div>
 
-
-          <div className="p-4 bg-gray-100 rounded-xl border cursor-pointer ransition-all duration-200 hover:bg-gray-200 hover:shadow-md hover:translate-y-1">
+          {/* Bank name secton  */}
+          <div className="p-4 bg-gray-100 rounded-xl border cursor-pointer ransition-all duration-200 hover:bg-gray-200 hover:shadow-md hover:translate-y-1"
+            onClick={()=>{
+              setEditField("bank_name");
+              setEditValue(singleRecipient?.Recipient?.bank_name);
+            }}
+          >
           <p className="text-gray-500 text-sm">Bank Name</p>
-          <p className="text-gray-800 font-semibold">"{singleRecipient?.Recipient?.bank_name ?? "Null"}"</p>
+            {editField === 'bank_name' ? (
+              <input
+                className='w-full p-2 border rounded-lg bg-white'
+                autoFocus
+                value={editValue}
+                onChange={(e)=> setEditValue(e.target.value)}
+                 onBlur={() => {
+                updateRecipient("bank_name", editValue);
+                setEditField(null);
+            }}
+              />
+            ):(
+              <p className="text-gray-800 font-semibold">"{singleRecipient?.Recipient?.bank_name ?? "Null"}"</p>
+            )}
           </div>
 
-
-          <div className="p-4 bg-gray-100 rounded-xl border cursor-pointer ransition-all duration-200 hover:bg-gray-200 hover:shadow-md hover:translate-y-1">
+            {/* Account number  */}
+          <div className="p-4 bg-gray-100 rounded-xl border cursor-pointer ransition-all duration-200 hover:bg-gray-200 hover:shadow-md hover:translate-y-1"
+            onClick={()=>{
+              setEditField('bank_account');
+              setEditValue(singleRecipient?.Recipient?.bank_account);
+            }}
+          >
           <p className="text-gray-500 text-sm">Account Number</p>
-          <p className="text-gray-800 font-semibold">{singleRecipient?.Recipient?.bank_account ?? "Null"}</p>
+          {editField === 'bank_account' ? (
+            <input
+              className='w-full p-2 border rounded-lg bg-white'
+              autoFocus
+              value={editValue}
+              onChange={(e)=> setEditValue(e.target.value)}
+               onBlur={() => {
+              updateRecipient("bank_account", editValue);
+              setEditField(null);
+            }}
+            />
+          ) : (
+              <p className="text-gray-800 font-semibold">{singleRecipient?.Recipient?.bank_account ?? "Null"}</p>
+          )}
+        
           </div>
 
-
-          <div className="p-4 bg-gray-100 rounded-xl border cursor-pointer ransition-all duration-200 hover:bg-gray-200 hover:shadow-md hover:translate-y-1">
+          {/* Address section  */}
+          <div className="p-4 bg-gray-100 rounded-xl border cursor-pointer ransition-all duration-200 hover:bg-gray-200 hover:shadow-md hover:translate-y-1"
+            onClick={()=>{
+              setEditField('address')
+              setEditValue(singleRecipient?.Recipient?.address)
+            }}
+          >
           <p className="text-gray-500 text-sm">Address</p>
-          <p className="text-gray-800 font-semibold">{singleRecipient?.Recipient?.address ?? "Null"}</p>
+          {editField === 'address' ? (
+            <input
+              className='w-full p-2 border rounded-lg bg-white'
+              autoFocus
+              value={editValue}
+              onChange={(e)=> setEditValue(e.target.value)}
+               onBlur={() => {
+              updateRecipient("address", editValue);
+              setEditField(null);
+            }}
+            />
+          ) : (
+             <p className="text-gray-800 font-semibold">{singleRecipient?.Recipient?.address ?? "Null"}</p>
+          )}
+
           </div>
 
-
-          <div className="p-4 bg-gray-100 rounded-xl border cursor-pointer ransition-all duration-200 hover:bg-gray-200 hover:shadow-md hover:translate-y-1">
+          {/* Email section  */}
+          <div className="p-4 bg-gray-100 rounded-xl border cursor-pointer ransition-all duration-200 hover:bg-gray-200 hover:shadow-md hover:translate-y-1"
+            onClick={()=>{
+              setEditField('email');
+              setEditValue(singleRecipient?.Recipient?.email);
+            }}
+          >
           <p className="text-gray-500 text-sm">Email</p>
-          <p className="text-gray-800 font-semibold">{singleRecipient?.Recipient?.email ?? "Null"}</p>
+          {editField === 'email' ? (
+            <input
+              className='w-full p-2 border rounded-lg bg-white'
+              autoFocus
+              value={editValue}
+              onChange={(e)=> setEditValue(e.target.value)}
+               onBlur={() => {
+              updateRecipient("email", editValue);
+              setEditField(null);
+            }}
+            />
+          ) : (
+           <p className="text-gray-800 font-semibold">{singleRecipient?.Recipient?.email ?? "Null"}</p>
+          )
+        }
           </div>
 
-
-          <div className="p-4 bg-gray-100 rounded-xl border cursor-pointer ransition-all duration-200 hover:bg-gray-200 hover:shadow-md hover:translate-y-1">
+          {/* Phone number section  */}
+          <div className="p-4 bg-gray-100 rounded-xl border cursor-pointer ransition-all duration-200 hover:bg-gray-200 hover:shadow-md hover:translate-y-1"
+            onClick={()=>{
+              setEditField('phone')
+              setEditValue(singleRecipient?.Recipient?.phone)
+            }}
+          >
           <p className="text-gray-500 text-sm">Phone</p>
-          <p className="text-gray-800 font-semibold">{singleRecipient?.Recipient?.phone ?? "Null"}</p>
+          {editField === 'phone' ? (
+            <input
+              className='w-full p-2 border rounded-lg bg-white'
+              autoFocus
+              value={editValue}
+              onChange={(e)=> setEditValue(e.target.value)}
+               onBlur={() => {
+              updateRecipient("phone", editValue);
+              setEditField(null);
+            }}
+            />
+          ) : (
+              <p className="text-gray-800 font-semibold">{singleRecipient?.Recipient?.phone ?? "Null"}</p>
+          )}
+        
           </div>
 
-
-          <div className="p-4 bg-gray-100 rounded-xl border cursor-pointer ransition-all duration-200 hover:bg-gray-200 hover:shadow-md hover:translate-y-1">
+            {/* City Section  */}
+          <div className="p-4 bg-gray-100 rounded-xl border cursor-pointer ransition-all duration-200 hover:bg-gray-200 hover:shadow-md hover:translate-y-1"
+            onClick={()=>{
+              setEditField('city')
+              setEditValue(singleRecipient?.Recipient?.city)
+            }}
+          >
           <p className="text-gray-500 text-sm">City</p>
-          <p className="text-gray-800 font-semibold">{singleRecipient?.Recipient?.city ?? "Null"}</p>
+          {editField === 'city' ? (
+            <input
+              className='w-full p-2 border rounded-lg bg-white'
+              autoFocus
+              value={editValue}
+              onChange={(e)=> setEditValue(e.target.value)}
+               onBlur={() => {
+              updateRecipient("city", editValue);
+              setEditField(null);
+            }}
+            />
+          ) : (
+             <p className="text-gray-800 font-semibold">{singleRecipient?.Recipient?.city ?? "Null"}</p>
+          )}
+
           </div>
 
-
-          <div className="p-4 bg-gray-100 rounded-xl border cursor-pointer ransition-all duration-200 hover:bg-gray-200 hover:shadow-md hover:translate-y-1">
+           {/* Wallet type section  */}
+          <div className="p-4 bg-gray-100 rounded-xl border cursor-pointer ransition-all duration-200 hover:bg-gray-200 hover:shadow-md hover:translate-y-1"
+            onClick={()=>{
+              setEditField('wallet_type');
+              setEditValue(singleRecipient?.Recipient?.wallet_type);
+            }}
+          >
           <p className="text-gray-500 text-sm">Wallet Type</p>
-          <p className="text-gray-800 font-semibold capitalize">{singleRecipient?.Recipient?.wallet_type ?? "Null"}</p>
+          {editField === 'wallet_type' ? (
+            <input
+              className='w-full p-2 border rounded-lg bg-white'
+              autoFocus
+              value={editValue}
+              onChange={(e)=>setEditValue(e.target.value)}
+               onBlur={() => {
+              updateRecipient("wallet_type", editValue);
+              setEditField(null);
+            }}
+            />
+          ) : (
+            <p className="text-gray-800 font-semibold capitalize">{singleRecipient?.Recipient?.wallet_type ?? "Null"}</p>
+          )}
+          
           </div>
 
-
-          <div className="p-4 bg-gray-100 rounded-xl border cursor-pointer ransition-all duration-200 hover:bg-gray-200 hover:shadow-md hover:translate-y-1">
+          {/* Wallet Number section  */}
+          <div className="p-4 bg-gray-100 rounded-xl border cursor-pointer ransition-all duration-200 hover:bg-gray-200 hover:shadow-md hover:translate-y-1"
+           onClick={()=>{
+              setEditField('wallet_number')
+              setEditValue(singleRecipient?.Recipient?.wallet_number)
+           }}
+          >
           <p className="text-gray-500 text-sm">Wallet Number</p>
-          <p className="text-gray-800 font-semibold">{singleRecipient?.Recipient?.wallet_number ?? 'Null'}</p>
+           {editField === 'wallet_number' ? (
+              <input
+                className='w-full p-2 border rounded-lg bg-white'
+                autoFocus
+                value={editValue}
+                onChange={(e)=> setEditValue(e.target.value)}
+                 onBlur={() => {
+              updateRecipient("wallet_number", editValue);
+              setEditField(null);
+            }}
+              />
+           ) : (
+             <p className="text-gray-800 font-semibold">{singleRecipient?.Recipient?.wallet_number ?? 'Null'}</p>
+           )}
+         
           </div>
           {/* </div> */}
           {/* ))} */}
